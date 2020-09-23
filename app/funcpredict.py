22b4583232f5782e171e48a04e685e18c5acecbd
Nguyễn Văn Minh <minhnv@sonek.vn>
2020-09-23 18:13:52 +0700

    ok

--- app/funcpredict.py
+++ app/funcpredict.py
@@ -102,12 +102,12 @@ def predict(filename, is_compareface=False, facelive=''):
     start = time.time()
 
     cccd_result = cccd.predictcccd(channel,stub, filename)
-    if 'id' in cccd_result and 'full_name' in cccd_result:
+    if cccd_result != None and 'id' in cccd_result and 'full_name' in cccd_result:
         printInfo(cccd_result,is_compareface,facelive)
     else:
         print("check image is cmnd")
         cmnd_result = cmnd.predictcmnd(channel,stub, filename)
-        if 'id' in cmnd_result:
+        if cmnd_result != None and 'id' in cmnd_result:
             printInfo(cmnd_result,is_compareface,facelive)
         else:
             print("Not detect image")
